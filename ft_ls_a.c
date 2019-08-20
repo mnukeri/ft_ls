5d96a11dab9bd776039c82d782902a4ab35e6ab1
Mulunghisi NUKERI <mnukeri@c5r8s10.wethinkcode.co.za>
2019-08-20 18:53:28 +0200

    LS rebuild

--- ft_ls_a.c
+++ ft_ls_a.c
@@ -1,14 +1,17 @@
 #include <dirent.h>
 #include "libft/libft.h"
 #include <sys/types.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-int			ft_array_swap(char *s1, char *s2, int m)
+int			ft_array_swap(char *s1, char *s2)
 {
 	char	*temp;
 
-	//if (s1 == NULL || s2 == NULL || m == 0)
-	//	return (0);
-	temp = (char*)malloc(sizeof(char) * m);
+	if (s1 == NULL || s2 == NULL)
+		return (0);
+	temp = ft_memalloc(100);
 	ft_strcpy(temp, s1);
 	ft_strcpy(s1, s2);
 	ft_strcpy(s2, temp);
@@ -16,7 +19,7 @@ int			ft_array_swap(char *s1, char *s2, int m)
 	return (1);
 }
 
-int			ft_sorter(char **s, int m)
+int			ft_sorter(char **s)
 {
 	int		p;
 	int		k;
@@ -29,7 +32,7 @@ int			ft_sorter(char **s, int m)
 		k = ft_strcmp(s[p - 1], s[p]);
 		if (k == 1)
 		{
-			if (ft_array_swap(s[p - 1], s[p], m) != 1)
+			if (ft_array_swap(s[p - 1], s[p]) != 1)
 				return (0);
 			p = 0;
 		}
@@ -41,12 +44,21 @@ int			ft_sorter(char **s, int m)
 int			ft_content_display(char **s)
 {
 	int		p;
+	int		j;
 
 	if (s == NULL)
 		return (0);
 	p = 0;
 	while (s[p])
-		ft_putendl(s[p++]);
+	{
+		j = 0;
+		while (s[p][j])
+			write(1, &s[p][j++], 1);
+		write(1, "\n", 1);
+		p++;
+		if (s[p] == 0)
+			return (1);
+	}
 	return (1);
 }
 
@@ -64,17 +76,11 @@ int			main(int argc, char **argv)
 	if ((pDir = opendir(argv[1])) == NULL)
 		return (0);
 	p = 0;
-	k = 0;
 	while ((pDirent = readdir(pDir)) != NULL)
-	{
-		if ((int)ft_strlen(pDirent->d_name) > k)
-			k = ft_strlen(pDirent->d_name);
 		p++;
-	}
 	closedir(pDir);
-	k += 1;
 	count = p + 1;
-	if (!(st = (char**)malloc(sizeof(char*) * count)))
+	if (!(st = ft_memalloc_2d(count)))
 		return (0);
 	ft_bzero(st, count);
 	p = 0;
@@ -82,18 +88,18 @@ int			main(int argc, char **argv)
 		return (0);
 	while ((pDirent = readdir(pDir)) != NULL)
 	{
-		//k = ft_strlen(pDirent->d_name);
-		//k += 1;
+		k = ft_strlen(pDirent->d_name);
+		k += 1;
 		if (!(st[p] = ft_memalloc(k)))
 			return (0);
 		ft_strcpy(st[p], pDirent->d_name);
 		p++;
 	}
-	closedir(pDir);
 	if (ft_content_display(st) != 1)
 		return (0);
-	if (ft_sorter(st, k) != 1)
+	if (ft_sorter(st) != 1)
 		return (0);
 	free(st);
+	closedir(pDir);
 	return (0);
 }
