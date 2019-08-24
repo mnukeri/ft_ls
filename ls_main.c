2caf9acc85ff3650f4836946691b09e8bd72072b
Mulunghisi NUKERI <mnukeri@c5r8s10.wethinkcode.co.za>
2019-08-24 16:43:32 +0200

    ls main

---
+++ ls_main.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ls_main.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mnukeri <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/24 16:37:38 by mnukeri           #+#    #+#             */
+/*   Updated: 2019/08/24 16:43:18 by mnukeri          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_ls.h"
+
+int		main(int argc, char **argv)
+{
+	char		**s;
+	int			k;
+
+	if (argc < 2)
+		return (0);
+	if ((k = ft_dir_count(&argv[1])) < 0)
+	{
+		ft_putendl("Could not count elements in directory;");
+		return (0);
+	}
+	if (!(s = ft_dircopy(&argv[1], k)))
+	{
+		ft_putendl("Could not copy elements into array;");
+		return (0);
+	}
+	if (ft_sorter(s) != 1)
+	{
+		ft_putendl("Could not sort;");
+		return (0);
+	}
+	if (ft_content_dispay(s) != 1)
+	{
+		ft_putendl("Could not display content after sorting");
+		return (0);
+	}
+	free(s);
+	return (0);
+}
